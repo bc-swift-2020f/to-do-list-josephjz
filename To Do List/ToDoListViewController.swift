d10d7164c6ef7c6d0b9e593f205d3494772fd01f
Jennifer Joseph <josephjz@bc.edu>
2020-09-28 00:08:36 -0400

    5.5 Complete

--- To Do List/ToDoListViewController.swift
+++ To Do List/ToDoListViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class ToDoListViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var addBarButton: UIBarButtonItem!
     
     // class wide arary variable to hold the To Do items for the app
     var toDoArray = ["Learn Swift", "Build Apps", "Change the World", "Take a Vacation"]
@@ -66,6 +67,23 @@ class ToDoListViewController: UIViewController {
             tableView.scrollToRow(at: newIndexPath, at: .bottom, animated: true)
         }
     }
+    
+    
+    @IBAction func editButtonPressed(_ sender: UIBarButtonItem) {
+        // check if table view is in editing mode
+        if tableView.isEditing {
+            //opposite of else block
+            tableView.setEditing(false, animated: true)
+            sender.title = "Edit"
+            addBarButton.isEnabled = true
+            
+        } else {
+            // want to set tableView.isEditing to true, set title of editing button to done using sender as button that was clicked on (aka edit), disable the add bar button
+            tableView.setEditing(true, animated: true)
+            sender.title = "Done"
+            addBarButton.isEnabled = false
+        }
+    }
 }
 
 
@@ -84,6 +102,28 @@ extension ToDoListViewController: UITableViewDelegate, UITableViewDataSource {
         cell.textLabel?.text = toDoArray[indexPath.row]
         print("cellForRowAt was just called, for indexPath = \(indexPath.row) which is the  cell containing \(toDoArray[indexPath.row])")
         return cell
+    }
+    
+    
+    // works for both insertion and deletion
+    // asks the data source to commit the insertion or deletion of a specified row in the receiver
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // index value of element to remove is passed in as arg
+            toDoArray.remove(at: indexPath.row)
+            // now element is removed from array, and need to remove row from table view
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        }
+    }
+    
+
+    
+    func tableView(_ tableView: UITableView, moveRowAt sourceIndexPath: IndexPath, to destinationIndexPath: IndexPath) {
+        // need to move data by deleting data from source index path.row, and insert the data at destination indexpath. row
+        // need a copy of data since we delete it first
+        let itemToMove = toDoArray[sourceIndexPath.row]
+        toDoArray.remove(at: sourceIndexPath.row)
+        toDoArray.insert(itemToMove, at: destinationIndexPath.row)
     }
     
     
