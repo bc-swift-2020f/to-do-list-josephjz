82d358f8debec9fd57d35ef09af11bd9f89179cd
Jennifer Joseph <josephjz@bc.edu>
2020-10-01 23:13:46 -0400

    5.10 Complete!

--- To Do List/AppDelegate.swift
+++ To Do List/AppDelegate.swift
@@ -1,19 +1,23 @@
-//
 //  AppDelegate.swift
 //  To Do List
-//
 //  Created by Jennifer Joseph on 9/22/20.
-//
+
+
+// file automatically has import UIKit, but we need to add import UserNotifications
 
 import UIKit
+import UserNotifications
+
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
-
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // allows notifications center to send messages that trigger functions in our app
+        // gives notification center on device right to send messages that are intercepted and received by our app so that we can respond to them
+        UNUserNotificationCenter.current().delegate = self
+                
         return true
     }
 
@@ -34,3 +38,29 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 }
 
+
+// need to add this extension!
+
+extension AppDelegate: UNUserNotificationCenterDelegate {
+
+    // code completion tells us that this function asks the delegate how to handle a notification that arrived while the app was running in the foreground
+    // this is IMPORTANT because if you were to get a notification from the app, while you're in the app, that notification won't show up
+    
+    // if we want app to show notifications when it is open (almost always do!); aka ensures notification will show when app is in the foreground
+    // close to the same from app to app
+    // need to add userNotificationCenter function
+    
+    func userNotificationCenter(_ center: UNUserNotificationCenter, willPresent notification: UNNotification, withCompletionHandler completionHandler: @escaping (UNNotificationPresentationOptions) -> Void) {
+        
+        // this code is useful because it gives you a heads up when a notification has been recieved, and its about to be displayed in a running app
+        
+        let id = notification.request.identifier            // each notification has a unique id
+        print("Recieved in-app notification with notification ID: \(id)")
+        
+        // don't want notifications lingering once the notification has been delivered
+        UNUserNotificationCenter.current().removeAllDeliveredNotifications()
+        
+        // elements of array represent different types of notifications that the app offers
+        completionHandler([.alert,.sound])
+    }
+}
--- To Do List/ToDoListViewController.swift
+++ To Do List/ToDoListViewController.swift
@@ -3,6 +3,7 @@
 //  Learning to work with Table Views in our apps
 
 import UIKit
+import UserNotifications    // MAKE SURE TO INCLUDE THIS LINE FOR FRAMEWORK
 
 // change the class name of ViewController in this app because we are
 // going to have multiple view controllers; this is going to show list of data in
@@ -24,19 +25,120 @@ class ToDoListViewController: UIViewController {
         
         // this line tells storyboard that this file will be the data source for the tableView object
         tableView.dataSource = self
+        
         // also will be delegate code for storyboard
         tableView.delegate = self
         
         loadData()
+        authorizeLocalNotifications()
+    }
+    
+    
+    func setNotifications() {
+        
+        // dont want to run this if there are no items in toDoItems List
+        guard toDoItems.count > 0 else {
+            return
+        }
+        //remove all notifications
+        UNUserNotificationCenter.current().removeAllPendingNotificationRequests()
+        //now we recreate the notifications with the data we just saved
+        
+        // note if we did for item in toDoItems we couldnt change item because it wpuld be a constant so instead we use index to access the data and change it
+        for index in  0..<toDoItems.count {
+            if toDoItems[index].reminderSet {
+                let toDoItem = toDoItems[index]
+                toDoItems[index].notificationID = setCalendarNotification(title: toDoItem.name, subtitle: "", body: toDoItem.notes, badgeNumber: nil, sound: .default, date: toDoItem.date)
+            }
+            
+        }
+        
+    }
+    
+    
+    
+    
+    
+    
+    // the values set for .title,subtitle,and body are the only 3 modifications specific to this app
+    // if you want a diff sound you have to make a custom sound using variable and UNNotificationSoundName, and UNNotificationName
+    // audio data is a lil finicky, have to drag .wav, .aiff, or .caf and have to be < 30 second
+    // note that it returns a String and this is the unique identifier of each notification
+    func setCalendarNotification(title: String, subtitle: String, body: String, badgeNumber: NSNumber?, sound: UNNotificationSound?, date: Date) -> String {
+        
+        // create content
+        // first create empty var content of type UNMutableNotificationContent and then populate it
+        let content = UNMutableNotificationContent()
+        content.title = title
+        content.subtitle = subtitle
+        content.body = body
+        content.sound = sound
+        content.badge = badgeNumber
+        
+        //create trigger -- pass in date components
+        // first line breaks up date conviently as day, month year
+        var dateComponents = Calendar.current.dateComponents([.year,.month,.day, .hour, .minute], from: date)
+        dateComponents.second = 00
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+        
+        //create request
+        let notificationID = UUID().uuidString
+        let request = UNNotificationRequest(identifier: notificationID, content: content, trigger: trigger)
+        
+        //register request with the notification center
+        UNUserNotificationCenter.current().add(request) { (error) in
+            // means error is not nil
+            if let error = error {
+                print("ERROR: \(error.localizedDescription) YIKES MAN, adding notification request went wrong")
+            } else {
+                print("Notification scheduled \(notificationID), title: \(content.title)")
+            }
+        }
+        
+        return notificationID
+    }
+    
+    
+    // this code gets user authorization to send them notifications
+    // pass in the kinds of notifications that will be authorized
+    // remember that UNUserNotificationCenter.current() go together
+    // when code completion comes up, double click bool part to change it to "trailing closure" because last parameter in method is a closure, aka block of code that we operate on after we get a bool or an optionally wrapped error; therefore code has to decide what to do when given either one of these
+
+    // granted for true or false that authorization has been given
+    func authorizeLocalNotifications() {
+        UNUserNotificationCenter.current().requestAuthorization(options: [.alert,.badge,.sound]) { (granted, error) in
+            
+            // code for error
+            // want to deal w error if error is not nil
+            // guards and makes sure that the only thing that pass are if the error is nil (no error)
+            // else happens if there is a error
+            guard error == nil else {
+                print("ERROR: \(error!.localizedDescription)")
+                return
+            }
+            
+            //code for bool
+            if granted {
+                print("notification authorization granted" )
+                
+            } else {
+                print("user denied notifications")
+                // TODO: add alert later on telling the user what to do
+                
+            }
+        }
     }
     
     
     func loadData() {
+        
         let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
         let documentURL = directoryURL.appendingPathComponent("todos").appendingPathExtension("json")
+        
         // read in data at this document url inside of let guard statement
         guard let data = try? Data(contentsOf: documentURL) else {return}
         let jsonDecoder = JSONDecoder()
+       
         do  {
             toDoItems = try jsonDecoder.decode(Array<ToDoItem>.self, from: data)
             tableView.reloadData()
@@ -47,34 +149,43 @@ class ToDoListViewController: UIViewController {
     
     
     
-    
     //go through file manager for saving directory location
     func saveData() {
+        
         let directoryURL = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!
         let documentURL = directoryURL.appendingPathComponent("todos").appendingPathExtension("json")
         let jsonEncoder = JSONEncoder()
         let data = try? jsonEncoder.encode(toDoItems)
+       
         do {
             try data?.write(to: documentURL, options: .noFileProtection)
         } catch {
             print("😡Error: Could not save data \(error.localizedDescription)")
         }
+        
+        setNotifications()
+   
     }
     
      
     
     // function passes in UIStoryboardSegue, triggered automatically just before segue happens in this viewController
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
         // ShowDetail is the Identifier name we gave to segue from to do list to scene for adding event
         if segue.identifier == "ShowDetail" {
+            
             // constant to hold destination
             // using as! allows us to set name of subclass so we are using file we want
             // destination is the ToDoDetailTableViewController which has "ShowDetail" which we are referencing
             let destination = segue.destination as! ToDoDetailTableViewController
             let selectedIndexPath = tableView.indexPathForSelectedRow!
+           
             //destination.toDoItem = toDoArray[selectedIndexPath.row]
             destination.toDoItem = toDoItems[selectedIndexPath.row]
+        
         } else {
+            
             // we only have 2 segues, so the other is "AddDetail"
             // selectedIndexPath is telling us if we have existing selection, and then deselect if we do
             if let selectedIndexPath = tableView.indexPathForSelectedRow {
@@ -84,8 +195,6 @@ class ToDoListViewController: UIViewController {
     }
     
     
-    
-    //
     @IBAction func unwindFromDetail(segue: UIStoryboardSegue) {
         let source = segue.source as! ToDoDetailTableViewController
         // if the table view has an existing row selection, because then we want to update this row
